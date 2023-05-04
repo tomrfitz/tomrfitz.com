e2997c1acf7bcbda80aca504fd34bd0d7cd2bcf8
Thomas FitzGerald <tomrfitz@gmail.com>
2023-05-04 15:34:18 -0400

    empty navbar footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type footerProps = {};
+
+const Footer: React.FC<footerProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default Footer;
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type navbarProps = {};
+
+const Navbar: React.FC<navbarProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default Navbar;
