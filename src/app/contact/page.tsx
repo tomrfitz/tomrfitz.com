d6c87cfb7f11e6aa4393fa70c7e894cd24951941
Thomas FitzGerald <tomrfitz@gmail.com>
2023-05-07 18:55:01 -0400

    import from github pages site, commentout css

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,35 @@
+export default function Contact() {
+  return (
+    <>
+      <h1>Contact</h1>
+      <p>
+        Send me an email at
+        <a
+          href="mailto:
+      tomrfitz@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          tomrfitz@gmail.com
+        </a>
+        or follow me on twitter at
+        <a
+          href="https://twitter.com/tomr_fitz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @tomr_fitz
+        </a>
+        or on
+        <a
+          href="nostr:npub1t0mrftzeuu8jwkcfyftwzj4cdvsdfhkamayqqdl9lzqwudq8ja4qx57pkf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nostr
+        </a>
+        .
+      </p>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,70 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
+    <main>
+      <h1>Hello</h1>
+      <p>This is my website hosted with GitHub Pages.</p>
+      <p>I hope to use this to show what I&apos;m working on.</p>
+      <h2>What I&apos;m doing right now:</h2>
+      <p>
+        I&apos;m currently self-studying CS while working part-time and
+        freelancing. If you have any suggestions for things to study or projects
+        to try, let me know. My background is in linguistics and cogsci.
+      </p>
+      <p>
+        I have some experience with Python, MATLAB, and R, in descending
+        familiarity. I&apos;m currently working through
         <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          href="https://github.com/ossu/computer-science"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          OSSU
         </a>
-
+        and want to learn Rust as well.
+      </p>
+      <p>
+        My current goal is to learn enough to build a web app-which might be
+        hosted here later-to record
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          href="https://en.wikipedia.org/wiki/Cluster_headache"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          cluster headache
         </a>
-
+        attacks. As someone with the condition, I&apos;ve found that it&apos;s
+        difficult to explain the severity of the disruptions it creates. I want
+        to create a solution that works for me, and that maybe can help others
+        too.
+      </p>
+      <h3>Contact me</h3>
+      <p>
+        Send me an email at
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          href="mailto: tomrfitz@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
+          tomrfitz@gmail.com
         </a>
-
+        or follow me on twitter at
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          href="https://twitter.com/tomr_fitz"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          @tomr_fitz
         </a>
-      </div>
+        or on
+        <a
+          href="nostr:npub1t0mrftzeuu8jwkcfyftwzj4cdvsdfhkamayqqdl9lzqwudq8ja4qx57pkf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nostr
+        </a>
+        .
+      </p>{" "}
     </main>
-  )
+  );
 }
